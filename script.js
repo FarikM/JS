228eb375e6bef6eea1f7477d99bcac6432fef42e
Farik <farikmahmudjonov01@gmail.com>
2023-11-30 22:29:36 +0500

    go!

---
+++ script.js
@@ -0,0 +1,21 @@
+var pText = document.querySelectorAll('p');
+
+
+pText.forEach((item) => {
+    item.onclick = function () {
+        alert(item.innerHTML);
+    }
+})
+
+pText.forEach((element) => {
+    
+    element.ondblclick = function () {
+        let span = document.createElement('span');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        span.append(checkbox);
+        element.append(span);
+        checkbox.checked = true;
+    }
+});
+// Uy ishi : osha p ni ikki marta bosilsa alertga bu bosilgan dib chiqishi kere
